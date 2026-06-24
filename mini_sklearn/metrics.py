460ee1233fd950318f7f17a5f0c0d8e0bebf038f
Harsh-verma0426 <lionworld13913@gmail.com>
2026-06-24 20:23:32 +0530

    Added r2score and accuracy score

---
+++ mini_sklearn/metrics.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def r2_score(y_test, y_pred):
+    ssr = np.sum((y_test-y_pred)**2)
+    sst = np.sum((y_test - np.mean(y_test)))
+    return 1-(ssr/sst)
+
+
+def accuracy_score(y_test, y_pred):
+    return np.mean(y_test==y_pred)
