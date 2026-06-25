62d80383d5a0e1e6e85c10df1d9d505e5fbfc065
Harsh-verma0426 <lionworld13913@gmail.com>
2026-06-25 19:56:10 +0530

    Added KNN Classifier

--- mini_sklearn/neighbors.py
+++ mini_sklearn/neighbors.py
@@ -1,4 +1,5 @@
 import numpy as np
+from collections import Counter
 
 class KNeighborsRegressor:
   def __init__(self, n_neighbors):
@@ -21,5 +22,36 @@ class KNeighborsRegressor:
       neighbors = distances[:self.n_neighbors]
       avg = np.mean([target for _, target in neighbors])
       final.append(avg)
+
+    return np.array(final)
+
+
+
+class KNeighborsClassifier:
+  def __init__(self, n_neighbors):
+    self.n_neighbors = n_neighbors
+
+  def fit(self, X, y):
+    self.X_train = X
+    self.y_train = y
+    
+    if self.n_neighbors > len(self.X_train):
+      raise ValueError("n_neighbors cannot exceed number of training samples")
+
+    return self
+
+  def predict(self, X):
+    final = []
+    for i in range(len(X)):
+      distances = []
+      for j in range(len(self.X_train)):
+        distance = np.sqrt(np.sum((self.X_train[j]-X[i])**2))
+        distances.append((distance, self.y_train[j]))
+      
+      distances.sort(key=lambda x: x[0])
+      neighbors = distances[:self.n_neighbors]
+      labels = [label for _, label in neighbors]
+      mode = Counter(labels).most_common(1)[0][0]
+      final.append(mode)
 
     return np.array(final)
